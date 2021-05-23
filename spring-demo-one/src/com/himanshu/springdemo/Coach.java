4471ca2c09d52df1ffe7802411b5307842037ae1
h1m <himanshu.yadav643@gmail.com>
2021-05-23 10:46:51 +0530

    started spring

---
+++ spring-demo-one/src/com/himanshu/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.himanshu.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
---
+++ spring-demo-one/src/com/himanshu/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.himanshu.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// create the object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
