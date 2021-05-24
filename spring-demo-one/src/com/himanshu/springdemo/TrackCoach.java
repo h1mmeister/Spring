2b5789a225ae4581d27ea35c06ad75852dcd1026
h1m <himanshu.yadav643@gmail.com>
2021-05-24 12:03:25 +0530

    constructor dependency injection

--- spring-demo-one/src/com/himanshu/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/himanshu/springdemo/TrackCoach.java
@@ -1,10 +1,24 @@
 package com.himanshu.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	// define a private field for dependency
+	private FortuneService fortuneService;
+	
+	// define constructor for dependency injection
+	public TrackCoach(FortuneService theFortuneService ) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
