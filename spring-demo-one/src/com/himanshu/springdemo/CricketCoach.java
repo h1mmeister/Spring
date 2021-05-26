b49b7b9ada0ce1edb6701f403f9589658b8591d9
h1m <himanshu.yadav643@gmail.com>
2021-05-26 17:40:29 +0530

    setting literals using config file

--- spring-demo-one/src/com/himanshu/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/himanshu/springdemo/CricketCoach.java
@@ -3,17 +3,35 @@ package com.himanshu.springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
-	
+	private String emailAddress;
+	private String team;
+
 	// default constructor
 	public CricketCoach() {
 		System.out.println("Cricket");
 	}
 	
-	// setter method
+	// setters method
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
 
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	// getters method
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
 
 	@Override
 	public String getDailyWorkout() {
