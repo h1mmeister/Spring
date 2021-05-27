5fd11df71d388f0ebc239ededb80810dfdfa52b8
h1m <himanshu.yadav643@gmail.com>
2021-05-27 12:43:26 +0530

    understanding singleton scope of bean

---
+++ spring-demo-one/src/com/himanshu/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,24 @@
+package com.himanshu.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// check if these are the same beans
+		boolean result = (theCoach == alphaCoach);
+		
+		System.out.println("\nPointing to the same object: " + result);
+		
+		context.close();
+
+	}
+
+}
