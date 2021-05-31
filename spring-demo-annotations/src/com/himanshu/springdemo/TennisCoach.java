22c1495ee9113af92a5fa73c0e56e2c944979a36
h1m <himanshu.yadav643@gmail.com>
2021-05-31 20:03:52 +0530

    checking prototype bean scope

--- spring-demo-annotations/src/com/himanshu/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/himanshu/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.himanshu.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("myTennisCoach")
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
